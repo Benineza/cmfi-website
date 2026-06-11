0fc5185f2eade2dfb879b78285f3b0f96492725d
Benineza <benineza.moise@gmail.com>
2026-06-11 16:27:09 +0200

    FIX: Remove allowCredentials and allow all CORS origins

--- server/src/main/java/com/cmfirwanda/server/config/WebConfig.java
+++ server/src/main/java/com/cmfirwanda/server/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
+                // .allowCredentials(true)
                 .maxAge(3600);
     }
 }
